c95bd741ffa445096701b9eabadfa384c63dcec3
John Doe <sumansingha8654@gmail.com>
2024-03-18 21:43:18 +0530

    hello

---
+++ course-c-LBCL21-problems.c
@@ -0,0 +1,26 @@
+/*Write a program which does the following
+
+Declare a variable num and store a user defined input from the console in it
+Declare a variable a and initialise it to 0
+Use the while syntax to create a loop, output the following to the console
+Print a in separate lines as long as it is less than num.
+Increment a by 1 in each iteration.*/
+#include <stdio.h>
+
+int main() {
+
+  int num;
+  printf("Enter A Num:");
+  scanf("%d", &num);
+  
+  int a = 0;
+  
+  while (a < num )
+  {
+    printf("%d\n", a);
+    a = a+1;
+  }
+
+  
+}
+
